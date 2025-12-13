04fa2dc7656a09edf8a4328167109844529cb278
Shivam Sharma <shivamsharma220380@gmail.com>
2025-12-13 22:06:52 +0530

    deployment cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
 const app = require('./src/app')
 
-
+const cors = require('cors');
+app.use(cors({
+    origin: ["https://crm-ebon-two.vercel.app/"], // Your Vercel Domain
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 
 const PORT = process.env.PORT || 3000
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
